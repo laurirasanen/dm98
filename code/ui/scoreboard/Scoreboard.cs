03acf03a52e685d7ba78d2c445628930eca43288
laurirasanen <lauri@laurirasanen.com>
2021-03-05 21:09:30 +0200

    Just use Sandbox.UI.Scoreboard

--- code/ui/scoreboard/Scoreboard.cs
+++ code/ui/scoreboard/Scoreboard.cs
@@ -1,50 +1,11 @@
 ﻿using Sandbox;
 using Sandbox.UI;
-using System.Linq;
+using Sandbox.UI.Construct;
 
-public class Scoreboard : Panel
+public class Scoreboard : Sandbox.UI.Scoreboard<Sandbox.UI.ScoreboardEntry>
 {
-	List<ScoreboardEntry> entries = new();
-
 	public Scoreboard()
 	{
 		StyleSheet = StyleSheet.FromFile( "/ui/scoreboard/Scoreboard.scss" );
-		AddClass( "scoreboard" );
-
-		AddHeader();
-
-		Canvas = Add.PanelWithClass( "canvas" );
-
-		PlayerInfo.OnPlayerAdded += AddPlayer;
-		PlayerInfo.OnPlayerUpdated += UpdatePlayer;
-		PlayerInfo.OnPlayerRemoved += RemovePlayer;
-
-		foreach ( var player in PlayerInfo.All )
-		{
-			AddPlayer( player );
-		}
-	}
-
-	public void AddPlayer( PlayerInfo.Entry player )
-	{
-		var scoreboardEntry = new ScoreboardEntry( this );
-		entries.add( scoreboardEntry );
-		UpdatePlayer( player );
-	}
-
-	public void UpdatePlayer( PlayerInfo.Entry player )
-	{
-		var entry = entries.First(x => x.Entry == player);
-		if ( !entry )
-		{
-			AddPlayer( player );
-			return;
-		}
-		entry.UpdateFrom( player );
-	}
-
-	public void RemovePlayer( PlayerInfo.Entry player )
-	{
-		entries.Remove( x => x.Entry == player );
 	}
 }
