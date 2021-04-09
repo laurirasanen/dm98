9406b9e82420bfd7f84a66d26c876841da266101
Garry Newman <garrynewman@users.noreply.github.com>
2021-04-09 10:37:20 +0100

    Switch stylesheet loading

--- code/ui/Scoreboard.cs
+++ code/ui/Scoreboard.cs
@@ -2,10 +2,37 @@
 using Sandbox.UI;
 using Sandbox.UI.Construct;
 
-public class Scoreboard : Sandbox.UI.Scoreboard<Sandbox.UI.ScoreboardEntry>
+public class Scoreboard : Sandbox.UI.Scoreboard<ScoreboardEntry>
 {
 	public Scoreboard()
 	{
-		StyleSheet = StyleSheet.FromFile( "/ui/scoreboard/Scoreboard.scss" );
+		StyleSheet.Load( "/ui/Scoreboard.scss" );
+	}
+
+	protected override void AddHeader()
+	{
+		Header = Add.Panel( "header" );
+		Header.Add.Label( "player", "name" );
+		Header.Add.Label( "kills", "kills" );
+		Header.Add.Label( "deaths", "deaths" );
+		Header.Add.Label( "ping", "ping" );
+		Header.Add.Label( "fps", "fps" );
+	}
+}
+
+public class ScoreboardEntry : Sandbox.UI.ScoreboardEntry
+{
+	public Label Fps;
+
+	public ScoreboardEntry()
+	{
+		Fps = Add.Label( "", "fps" );
+	}
+
+	public override void UpdateFrom( PlayerScore.Entry entry )
+	{
+		base.UpdateFrom( entry );
+
+		Fps.Text = entry.Get<int>( "fps", 0 ).ToString();
 	}
 }
