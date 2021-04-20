207d9bb69d2b2922f715e8c3c7babcef41be030e
laurirasanen <lauri@laurirasanen.com>
2021-04-20 18:29:05 +0300

    WIP carriable code

---
+++ code/carriables/BaseCarriable.cs
@@ -0,0 +1,55 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class BaseCarriable : IRespawnableEntity
+{
+	public virtual int Weight => 0;
+
+	public virtual string ModelPath => "models/citizen_props/roadcone01.vdml";
+
+	[NetPredicted]
+	public TimeSince TimeSincePickup { get; set; }
+
+	[NetPredicted]
+	public TimeSince TimeSinceDrop { get; set; }
+
+	public PickupTrigger PickupTrigger { get; protected set; }
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( ModelPath );
+
+		PickupTrigger = new PickupTrigger();
+		PickupTrigger.Parent = this;
+		PickupTrigger.WorldPos = WorldPos;
+		TimeSinceDrop = 0;
+	}
+
+	public virtual void OnCarryStart( Entity carrier )
+	{
+		// TODO - check whats in BaseWeapon.OnCarryStart
+		if ( PickupTrigger.IsValid() )
+		{
+			PickupTrigger.EnableTouch = false;
+		}
+
+		TimeSincePickup = 0;
+	}
+
+	public virtual void OnCarryDrop( Entity dropper )
+	{
+		if ( PickupTrigger.IsValid() )
+		{
+			PickupTrigger.EnableTouch = true;
+		}
+
+		TimeSinceDrop = 0;
+	}
+}
---
+++ code/carriables/MoneyBag.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+partial class MoneyBag : BaseCarriable
+{
+	public override int Weight => 10;
+	public override string ModelPath => "models/citizen_props/roadcone01.vdml";
+}
